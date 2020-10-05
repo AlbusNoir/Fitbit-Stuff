d8930b84b61633d79bf647fc7e288c8c21e69194
Kale <13501991+AlbusNoir@users.noreply.github.com>
2020-10-05 18:52:03 -0400

    added digital display, handling for 12h 24h time, added button for 2ndscreen

--- Analog-Face1/app/index.js
+++ Analog-Face1/app/index.js
@@ -1,12 +1,63 @@
 import document from "document";
 import clock from "clock";
+import {preferences} from "user-settings";
+import * as util from "../common/utils"
 
 //Update every sec
 clock.granularity = "seconds";
 
+//analog vars
 const secHand = document.getElementById("secs");
 const minHand = document.getElementById("mins");
 const hourHand = document.getElementById("hours");
+
+//digital vars
+const hours1 = document.getElementById("hours1");
+const hours2 = document.getElementById("hours2");
+const separator = document.getElementById("separator");
+const mins1 = document.getElementById("mins1");
+const mins2 = document.getElementById("mins2");
+
+//stats screen
+const btn = document.getElementById("btn");
+const screen2 = document.getElementById("screen2");
+
+btn.onclick = function(){
+  toggle(screen2);
+}
+
+//toggle element passed in via "enable" id
+function toggle(element){
+  element.animate("enable");
+}
+
+
+// separator blink. take val, mod2, if 0 display, if not, don't. val is sec
+function separatorBlink(val){
+  separator.style.display = (val % 2 === 0 ? "inline": "none");
+}
+
+function drawDigit(val, place){
+  // update img with dynamic href
+  place.image = `Font/${val}.png`;
+}
+
+function setHours(val){
+  //digit 1
+  if(val > 9){
+    drawDigit(Math.floor(val / 10), hours1);
+  } else {
+    drawDigit("", hours1);
+  }
+  // digit2
+  drawDigit(Math.floor(val % 10), hours2);
+}
+
+function setMins(val){
+  drawDigit(Math.floor(val / 10), mins1);
+  drawDigit(Math.floor(val % 10), mins2);
+}
+
 
 // ToAngle funcs
 function timeToAngle(time){
@@ -75,5 +126,28 @@ function hoursToAngle(hours){
 
 //update
 clock.ontick = evt =>{
+  
+  const d = evt.date;
+  
+  const hours = d.getHours();
+  
+  const minutes = ("0" + d.getMinutes()).slice(-2); // append 0 so we can have 01 02 03 etc
+  
+  // 24hr - 12hr toggle
+  if(preferences.clockDisplay === "12h") {
+    hours = hours % 12 || 12; // mod 12 else 12
+  } else {
+    hours = util.zeroPad(hours);
+  }
+  
+  // sep blink - remove if don't want blink
+  separatorBlink(d.getSeconds());
+  
+  // set digital time
+  setHours(hours);
+  
+  setMins(minutes);
+  
+  // set analog time
   updateClock();
 }
---
+++ Analog-Face1/common/utils.js
@@ -0,0 +1,8 @@
+export function zeroPad(i){
+  if(i < 10){
+    i = "0" + i;
+  }
+  return i;
+}
+
+//TODO: move more functions here
